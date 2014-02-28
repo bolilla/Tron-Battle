b0123fb00449e2aff89c3afbba4c2a369bd7817b
Borja Roux <borja.roux@gmail.com>
2014-02-28 23:52:07 +0100

    Player plays according to rules in codingame. Intelligence is based on the board domination, space and proximity to rivals.

--- tron.go
+++ tron.go
@@ -2,6 +2,7 @@
 package main
 
 import (
+	"bufio"
 	"bytes"
 	"fmt"
 	"os"
@@ -10,15 +11,17 @@ import (
 )
 
 const (
-	MAX_X  = 30
-	MAX_Y  = 20
-	MAX_XY = 30
-	DEBUG  = true
-	UP     = "UP"
-	DOWN   = "DOWN"
-	LEFT   = "LEFT"
-	RIGHT  = "RIGHT"
-	DIE    = "DIE" //when there is nowhere else to go
+	MAX_X              = 30
+	MAX_Y              = 20
+	MAX_XY             = 30
+	DEBUG              = false
+	TIME               = false
+	VORONOI_OVER_SPACE = 1.5
+	UP                 = "UP"
+	DOWN               = "DOWN"
+	LEFT               = "LEFT"
+	RIGHT              = "RIGHT"
+	DIE                = "DIE" //when there is nowhere else to go
 )
 
 var (
@@ -34,12 +37,15 @@ var (
 //Tries to win Tron games
 func main() {
 	b = newBoard()
+	if len(os.Args) == 2 {
+		readBoard(os.Args[1])
+	}
 	for i := 0; i < MAX_X*MAX_Y; i += 1 { //Just to avoid infinite loop
 		t0 := time.Now()
 		readInput()
 		fmt.Println(printMovement())
 		t1 := time.Now()
-		if DEBUG {
+		if DEBUG || TIME {
 			fmt.Printf("The call took %v to run.\n", t1.Sub(t0))
 		}
 	}
@@ -60,22 +66,18 @@ func newBoard() *board {
 func printMovement() (result string) {
 	if len(playersHeads) > 1 {
 		if DEBUG {
-			fmt.Println("BEFORE Board:")
+			fmt.Println("CURRENT Board:")
 			fmt.Println(b)
 		}
 		computeSituation()
-		if DEBUG {
-			fmt.Println("Voronoi:")
-			fmt.Println(v)
-			fmt.Println("Board:")
-			fmt.Println(b)
-			fmt.Println("Am I alone?", alone)
-		}
-		if alone && voronoiSizes[whoami] < 50 {
-			result = delayTheInevitable()
-		} else {
-			result = attack()
-		}
+		//if DEBUG {
+		//	fmt.Println("Voronoi:")
+		//	fmt.Println(v)
+		//	fmt.Println("Board:")
+		//	fmt.Println(b)
+		//	fmt.Println("Am I alone?", alone)
+		//}
+		result = doMove()
 	} else {
 		fmt.Println("Skipping step. I am alone. Nothing to think about...")
 		fmt.Println(b)
@@ -88,16 +90,30 @@ func computeSituation() {
 	computeVoronoi()
 }
 
+//Computes the space around me. It is calculated in the same way as the voronoi board, but only me is moving
+func computeSpace() {
+	v = newBoard()
+	var stepper chan bool = make(chan bool)
+	var notifier chan bool = make(chan bool)
+	go computeVoronoiPerPlayer(playersHeads[whoami].x, playersHeads[whoami].y, whoami, stepper, notifier)
+	var moreToReceive bool = true
+	for moreToReceive {
+		stepper <- true
+		_, moreToReceive = <-notifier
+	}
+}
+
+//Computes the distance to the nearest oponent. It is calculated in the same way as the voronoi board, only me is moving and when I find the head of another player, I stop.
+func computeDistance() int {
+	v = newBoard()
+	return computeDistanceFromPlayer(playersHeads[whoami].x, playersHeads[whoami].y, whoami)
+}
+
 //Computes voronoi board
 func computeVoronoi() {
-	v = new(board)
+	v = newBoard()
 	var steppers []chan bool = make([]chan bool, len(playersHeads))
 	var notifiers []chan bool = make([]chan bool, len(playersHeads))
-	for i := 0; i < len(v); i += 1 {
-		for j := 0; j < len(v[i]); j += 1 {
-			v[i][j] = -1
-		}
-	}
 	for i := 0; i < len(playersHeads); i += 1 {
 		steppers[i] = make(chan bool)
 		notifiers[i] = make(chan bool)
@@ -117,19 +133,44 @@ func computeVoronoi() {
 	}
 }
 
-//Returns true iff all values are false //TESTED
-func allFalse(input []bool) bool {
-	for _, val := range input {
-		if val {
-			return false
+//Computes the voronoi diagram from given point
+func computeDistanceFromPlayer(x, y, i int) int {
+	voronoiSizes[i] = 0
+	v[x][y] = i
+	var printed bool = true
+	possibilities := make(map[coordinate]bool)
+	possibilities[coordinate{x, y}] = true
+	voronoiSizes[i] += len(possibilities)
+	result := 1
+	for printed {
+		result += 1
+		printed = false
+		possibilities = b.possiblePositionsFromArray(possibilities)
+		for pos, _ := range possibilities {
+			for n, head := range playersHeads {
+				if n != whoami && (abs(pos.x-head.x)+abs(pos.y-head.y)) == 1 {
+					return result
+				}
+			}
+			if v[pos.x][pos.y] == -1 {
+				v[pos.x][pos.y] = i
+				printed = true
+			}
 		}
 	}
-	return true
+	return 1
+}
+
+//Returns the absolute value of an int
+func abs(i int) int {
+	if i < 0 {
+		return (-1) * i
+	}
+	return i
 }
 
 //Computes the voronoi diagram from given point
 func computeVoronoiPerPlayer(x, y, i int, stepper, notifier chan bool) {
-	//fmt.Println("Computing voronoi from", x, y, "for", i)
 	if i == whoami {
 		alone = true
 	}
@@ -148,7 +189,7 @@ func computeVoronoiPerPlayer(x, y, i int, stepper, notifier chan bool) {
 				v[pos.x][pos.y] = i
 				voronoiSizes[i] += 1
 				printed = true
-			} else if v[pos.x][pos.y] != whoami {
+			} else if i == whoami && v[pos.x][pos.y] != whoami {
 				alone = false
 			}
 		}
@@ -160,38 +201,46 @@ func computeVoronoiPerPlayer(x, y, i int, stepper, notifier chan bool) {
 	}
 }
 
-//Makes best move based on the fact that the player is inside a dead end
-func delayTheInevitable() string {
-	result, _ := b.getLongestMove(playersHeads[whoami])
-	return result
+//Returns true iff all values are false //TESTED
+func allFalse(input []bool) bool {
+	for _, val := range input {
+		if val {
+			return false
+		}
+	}
+	return true
 }
 
-//Returns the move that leads to a bigger set of movements (longest dead)
-//method is associated to board instead of *board to force the copy of the board when the function is called
-func (b board) getLongestMove(c coordinate) (string, int) {
-	b[c.x][c.y] = 9
-	possibilities := b.possiblePositions(c.x, c.y)
-	if len(possibilities) == 0 {
-		return "", 0
-	} else if len(possibilities) == 1 {
-		_, length := b.getLongestMove(possibilities[0])
-		return movement(possibilities[0], c), length + 1
-	}
-	lengths := make([]int, len(possibilities))
-	for i, pos := range possibilities {
-		_, lengths[i] = b.getLongestMove(pos)
-	}
-	longest := positionMaxValue(lengths)
-	return movement(possibilities[longest], c), longest
-}
+////Makes best move based on the fact that the player is inside a dead end
+//func delayTheInevitable() string {
+//	result, _ := b.getLongestMove(playersHeads[whoami])
+//	return result
+//}
+
+////Returns the move that leads to a bigger set of movements (longest dead)
+////method is associated to board instead of *board to force the copy of the board when the function is called
+//func (b board) getLongestMove(c coordinate) (string, int) {
+//	b[c.x][c.y] = 9
+//	possibilities := b.possiblePositions(c.x, c.y)
+//	if len(possibilities) == 0 {
+//		return "", 0
+//	} else if len(possibilities) == 1 {
+//		_, length := b.getLongestMove(possibilities[0])
+//		return movement(possibilities[0], c), length + 1
+//	}
+//	lengths := make([]int, len(possibilities))
+//	for i, pos := range possibilities {
+//		_, lengths[i] = b.getLongestMove(pos)
+//	}
+//	longest := positionMaxValue(lengths)
+//	return movement(possibilities[longest], c), longest
+//}
 
 //Moves towards tha position that maximizes the space to maneouver when alone and approaches strongest rival
-func attack() string {
+func doMove() string {
 	possibilities := b.possiblePositions(playersHeads[whoami].x, playersHeads[whoami].y)
 	if DEBUG {
-		fmt.Println("voronoiSizes", voronoiSizes)
 		fmt.Println("My possibilities", possibilities)
-		fmt.Println(b)
 	}
 
 	if len(possibilities) > 0 {
@@ -207,28 +256,91 @@ func attack() string {
 	}
 }
 
-//Returns the "space" that would be left in each of the options
+//Returns the "space" that would be left in each of the options.
 func computeOptionsValues(possibilities []coordinate) []int {
 	result := make([]int, len(possibilities))
+	pointsPerVoronoi := calculatePointsPerVoronoi(possibilities)
+	pointsPerSpace := calculatePointsPerSpace(possibilities)
+	pointsPerDistance := calculatePointsPerDistance(possibilities)
+	if DEBUG {
+		fmt.Println("Possibilities", possibilities)
+		fmt.Println("pointsPerVoronoi", pointsPerVoronoi)
+		fmt.Println("pointsPerSpace", pointsPerSpace)
+	}
+	for i, _ := range result {
+		result[i] = int((float32(pointsPerVoronoi[i])*VORONOI_OVER_SPACE)+float32(pointsPerSpace[i])) + (30 / pointsPerDistance[i])
+	}
+	return result
+}
+
+//Calculates the score of each possibility based on the distance to the nearest opponent
+func calculatePointsPerDistance(possibilities []coordinate) []int {
+	result := make([]int, len(possibilities))
 	for i, pos := range possibilities {
-		oldVal := b[pos.x][pos.y]
+		oldHead := playersHeads[whoami]
+		playersHeads[whoami] = coordinate{pos.x, pos.y}
 		b[pos.x][pos.y] = whoami
-		computeSituation()
+		result[i] = computeDistance()
 		if DEBUG {
+			fmt.Println("Computing option", pos)
 			fmt.Println("Board in this case")
 			fmt.Println(b)
-			fmt.Println("Optional v", i, ":", pos)
+			fmt.Println("Voronoi calculating distance")
+			fmt.Println(v)
+		}
+		b[pos.x][pos.y] = -1
+		playersHeads[whoami] = oldHead
+	}
+	return result
+}
+
+//Calculates the scrore of each possibility based on the space left after the move
+func calculatePointsPerSpace(possibilities []coordinate) []int {
+	result := make([]int, len(possibilities))
+	for i, pos := range possibilities {
+		oldHead := playersHeads[whoami]
+		playersHeads[whoami] = coordinate{pos.x, pos.y}
+		b[pos.x][pos.y] = whoami
+		computeSpace()
+		if DEBUG {
+			fmt.Println("Computing option", pos)
+			fmt.Println("Board in this case")
+			fmt.Println(b)
+			fmt.Println("Voronoi in this case")
 			fmt.Println(v)
 		}
 		result[i] = voronoiSizes[whoami]
-		b[pos.x][pos.y] = oldVal
+		b[pos.x][pos.y] = -1
+		playersHeads[whoami] = oldHead
+	}
+	return result
+}
+
+//Calculates the score of each possibility based on the voronoi size of the player in each move
+func calculatePointsPerVoronoi(possibilities []coordinate) []int {
+	result := make([]int, len(possibilities))
+	for i, pos := range possibilities {
+		oldHead := playersHeads[whoami]
+		playersHeads[whoami] = coordinate{pos.x, pos.y}
+		b[pos.x][pos.y] = whoami
+		computeSituation()
+		if DEBUG {
+			fmt.Println("Computing option", pos)
+			fmt.Println("Board in this case")
+			fmt.Println(b)
+			fmt.Println("Voronoi in this case")
+			fmt.Println(v)
+		}
+		result[i] = voronoiSizes[whoami]
+		b[pos.x][pos.y] = -1
+		playersHeads[whoami] = oldHead
 	}
 	return result
 }
 
 //Returns the position of the highest positive value //TESTED
 func positionMaxValue(vals []int) int {
-	var maxVal, result int
+	var maxVal, result int = -10000, -10000
 	for i, val := range vals {
 		if maxVal < val {
 			maxVal = val
@@ -307,7 +419,6 @@ func readInput() {
 		}
 		if newX == -1 || newY == -1 {
 			b.removeBeam(i)
-			voronoiSizes[i] = 0
 		} else {
 			b[newX][newY] = i
 			playersHeads[i].x = newX
@@ -354,10 +465,30 @@ func (b *board) removeBeam(p int) {
 	for y := 0; y < MAX_Y; y += 1 {
 		for x := 0; x < MAX_X; x += 1 {
 			if b[x][y] == p {
-				b[x][y] = 0
+				b[x][y] = -1
 			}
 		}
 	}
 }
 
 //Reads a board from a file
+func readBoard(filePath string) {
+	fmt.Println("Reading board")
+	f, err := os.Open(filePath)
+	defer f.Close()
+	if err != nil {
+		panic(err)
+	}
+	scanner := bufio.NewScanner(f)
+	for y := 0; y < MAX_Y; y += 1 {
+		scanner.Scan()
+		line := scanner.Text()
+		for x := 0; x < MAX_X && x < len(line); x += 1 {
+			if line[x] != 32 { //white space
+				b[x][y] = int(line[x] - 48) //Where numbers begin
+			}
+		}
+	}
+	fmt.Println("Read board:")
+	fmt.Println(b)
+}
